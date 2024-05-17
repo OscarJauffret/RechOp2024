2eb3432277bab85b77866b3aff99d43aa34e985f
kwojtach <kacper.wojtach@ulb.ac.be>
2024-05-17 23:53:36 +0200

    pays des abers_bis,  commentaires

--- Code/Pays_Abers_Bis.py
+++ Code/Pays_Abers_Bis.py
@@ -7,43 +7,50 @@ import time
 import numpy as np
 from matplotlib import pyplot as plt
 import math
+from utils import *
 
-ACCELERATED_MUTATION_THRESHOLD = 1000
+# The size of the population in each generation.
 POPULATION_SIZE = 1000
-WEIGHT_LIMIT = 5850
+
+# A placeholder value for bad solutions. This could be used to represent solutions that are not feasible or that have a very high cost.
 bad = 999999
-ACCELERATED_MUTATION_NUMBER = 3
-OLD_GENERATION = 200
+
+# The probability of mutation. This is the chance that an individual will undergo mutation in each generation.
 MUTATION_RATE = 0.9
+
+# Change the working directory to the data directory
 try:
     os.chdir("../Data/Probleme_Abers_2_bis/")
 except FileNotFoundError:
     pass
-# Load necessary data
-with open("init_sol_Abers_pb2_bis.pickle", "rb") as f:
-    init_solu = pickle.load(f)
-    init_solu = init_solu.transpose()
-    init_solu = init_solu.values.tolist()
-    init_solu = [item for sublist in init_solu for item in sublist]
 
-with open("dist_matrix_Abers_pb2_bis.pickle", "rb") as f:
-    dist_matrix = pickle.load(f)
+# Load the initial solution and transform it
+init_solu = load_data("init_sol_Abers_pb2_bis.pickle")
+init_solu = transform_data(init_solu)
 
-with open("weight_Abers_pb2_bis.pickle", "rb") as f:
-    weight_list = pickle.load(f)
+# Load the other data files
+dist_matrix = load_data("dist_matrix_Abers_pb2_bis.pickle")
+weight_list = load_data("weight_Abers_pb2_bis.pickle")
+dur_matrix = load_data("dur_matrix_Abers_pb2_bis.pickle")
+collection_time = load_data("temps_collecte_Abers_pb2_bis.pickle")
+bilat_pairs = load_data("bilat_pairs_Abers_pb2_bis.pickle")
 
-with open("dur_matrix_Abers_pb2_bis.pickle", "rb") as f:
-    dur_matrix = pickle.load(f)
-
-with open("temps_collecte_Abers_pb2_bis.pickle", "rb") as f:
-    collection_time = pickle.load(f)
-
-with open("bilat_pairs_Abers_pb2_bis.pickle", "rb") as f:
-    bilat_pairs = pickle.load(f)
-    
+# Create a dictionary from the bilateral pairs
+bilat_pairs_dict = create_dict(bilat_pairs)    
 
 def initialize_population(init_sol, population_size):
-    print(f"Génération 0: Solution initiale: {fitness(init_sol)} {init_sol}")
+    """
+    Initialize the population for the genetic algorithm.
+
+    The initial population is created by performing random mutations on the initial solution.
+
+    Parameters:
+    init_sol (list): The initial solution.
+    population_size (int): The size of the population to create.
+
+    Returns:
+    list: The initial population.
+    """
     population = [init_sol.copy()]
     for _ in range(1, population_size):
         new_individual = mutation(init_sol.copy(), random.randint(1, 5))
@@ -52,6 +59,18 @@ def initialize_population(init_sol, population_size):
 
 
 def fitness(chemin) -> tuple[float, int]:
+    """
+    Calculate the fitness of a solution.
+
+    The fitness is calculated as the total distance of the solution. 
+    The function also finds the maximum distance between any two points in the solution and its index.
+
+    Parameters:
+    chemin (list): The solution to evaluate.
+
+    Returns:
+    tuple: The total distance and the index of the maximum distance.
+    """
     total_distance = 0
     max_distance = 0
     index_max_distance = 0
@@ -65,27 +84,41 @@ def fitness(chemin) -> tuple[float, int]:
     return total_distance , index_max_distance
 
 
-def create_dict(pairs):
-    pair_dict = {}
-    for pair in pairs:
-        pair_dict[pair[0]] = pair[1]
-        pair_dict[pair[1]] = pair[0]
-    return pair_dict
-
-bilat_pairs_dict = create_dict(bilat_pairs)
-
 def selection(population, pool):
+    """
+    Select the best individuals from the population.
+
+    The fitness of each individual is calculated and the individuals are sorted based on their fitness. 
+    The top individuals are selected.
+
+    Parameters:
+    population (list): The population.
+    pool (Pool): The multiprocessing pool.
+
+    Returns:
+    list: The selected individuals.
+    """
     fitness_values = pool.map(fitness, population)
     ranked_solutions = sorted(zip(fitness_values, population), reverse=False)
     return ranked_solutions[:POPULATION_SIZE // 6]
 
 
-def calculateVariance(population):
-    mean = sum(sol[0][0] for sol in population) / len(population)
-    return sum((sol[0][0] - mean) ** 2 for sol in population) / len(population)
-
-
 def linear_base(variance, min_variance=50000, max_variance=180000, min_base=0.0005, max_base=1.01):
+    """
+    Calculate the base for the exponential decay function used in tournament selection.
+
+    The base is calculated based on the variance of the fitness values in the population.
+
+    Parameters:
+    variance (float): The variance of the fitness values.
+    min_variance (float): The minimum variance.
+    max_variance (float): The maximum variance.
+    min_base (float): The minimum base.
+    max_base (float): The maximum base.
+
+    Returns:
+    float: The calculated base.
+    """
     if variance <= min_variance:
         return min_base   
     elif variance >= max_variance:
@@ -95,6 +128,19 @@ def linear_base(variance, min_variance=50000, max_variance=180000, min_base=0.00
 
 
 def tournament_selection(population, variance, tournament_size=10):
+    """
+    Perform tournament selection.
+
+    A number of individuals are randomly selected from the population and the best one is chosen.
+
+    Parameters:
+    population (list): The population.
+    variance (float): The variance of the fitness values.
+    tournament_size (int): The number of individuals to select for the tournament.
+
+    Returns:
+    The selected individual.
+    """
     n = len(population)
     base = linear_base(variance)
     weights = [math.exp(-base * i) for i in range(n)]
@@ -102,6 +148,22 @@ def tournament_selection(population, variance, tournament_size=10):
 
 
 def crossover(parent1: list[int], parent2: list[int], parent_1_worst_gene: int, parent_2_worst_gene:  int) -> list[int]:
+    """
+    Perform crossover between two parents to create a child.
+
+    The child is created by taking segments from each parent. The segments are chosen based on the worst genes of each parent. 
+    The child inherits the rest of the genes from the parents in the order they appear.
+
+    Parameters:
+    parent1 (list[int]): The first parent.
+    parent2 (list[int]): The second parent.
+    parent_1_worst_gene (int): The index of the worst gene in the first parent.
+    parent_2_worst_gene (int): The index of the worst gene in the second parent.
+
+    Returns:
+    list[int]: The child created by crossover.
+    """
+    # Determine the segments to take from each parent
     if parent_1_worst_gene < parent_2_worst_gene:
         segment1 = parent1[1:parent_1_worst_gene]
         segment2 = parent2[parent_1_worst_gene:parent_2_worst_gene]
@@ -111,52 +173,100 @@ def crossover(parent1: list[int], parent2: list[int], parent_1_worst_gene: int, 
         segment2 = parent1[parent_2_worst_gene:parent_1_worst_gene]
         segment3 = parent2[parent_1_worst_gene:]
 
+    # Initialize the child and a set to keep track of the genes in the child
     child = [0]
     child_set = set(child)
 
+    # Add the genes from the segments to the child
     for segment in [segment1, segment2, segment3]:
         for gene in segment:
             if gene not in child_set:
                 child.append(gene)
                 child_set.add(gene)
 
+    # Add the remaining genes from the parents to the child
     for parent in [parent1, parent2]:
         for gene in parent:
             if gene not in child_set:
                 child.append(gene)
                 child_set.add(gene)
+
     return child
 
 
 def mutation(individual, length=3):
+    """
+    Perform mutation on an individual.
+
+    A segment of the individual is selected and potentially shuffled. 
+    The segment is then removed from the individual and inserted at a new random position. 
+    Before and after the removal and insertion, the function also swaps the genes at the boundaries 
+    of the segment with their pairs if they have any.
+
+    Parameters:
+    individual (list): The individual to mutate.
+    length (int): The length of the segment to mutate.
+
+    Returns:
+    list: The mutated individual.
+    """
+    # Select a random segment of the individual
     start = random.randint(1, len(individual) - 2 - length)
     end = start + length
     segment = individual[start:end]
+
+    # Possibly shuffle the segment
     if random.random() < 0.2:
         subset = segment[1:-1]
         random.shuffle(subset)
         segment[1:-1] = subset
+
+    # Swap the genes at the boundaries of the segment with their pairs if they have any
     if start-1 != 0:
         individual = swap_with_pair(individual, start -1)
     individual = swap_with_pair(individual, start)
     individual = swap_with_pair(individual, end-1)
     individual = swap_with_pair(individual, end)
-    #if random.random() < 0.5:
-    #    segment = segment[::-1]
+
+    # Remove the segment from the individual
     del individual[start:end]
+
+    # Insert the segment at a new random position
     new_position = random.randint(1, len(individual) - 2)
+
+    # Swap the genes at the boundaries of the new position with their pairs if they have any
     if new_position - 1 != 0:
         individual = swap_with_pair(individual, new_position - 1)
     individual = swap_with_pair(individual, new_position)
+
+    # Insert the segment at the new position
     individual = individual[:new_position] + segment + individual[new_position:]
+
     return individual
 
 def swap_with_pair(individual, index=-1):
+    """
+    Swap a gene in the individual with its pair.
+
+    The function selects a gene at a given index or at a random index if no index is provided. 
+    If the gene has a pair in the bilat_pairs_dict, it swaps the gene with its pair with a 20% probability.
+
+    Parameters:
+    individual (list): The individual to mutate.
+    index (int, optional): The index of the gene to swap. If no index is provided, a random index is selected. Default is -1.
+
+    Returns:
+    list: The mutated individual.
+    """
+    # Select a random index if no index is provided
     if index == -1:
         index = random.randint(1, len(individual) - 2)
+
+    # If the gene at the selected index has a pair, swap it with its pair with a 20% probability
     if individual[index] in bilat_pairs_dict:
         if random.random() < 0.2:
             individual[index] = bilat_pairs_dict[individual[index]]
+
     return individual
 
 
@@ -214,48 +324,47 @@ def genetic_algorithm(init_sol, population_size, best_scores, variances):
                 new_population.append(child)
 
             population = new_population
-
     return min(population, key=fitness)
 
 
-def calculateDandT(l):
-    distance = 0
-    time = 0
-    for i in range(len(l)):
-        if i != len(l) - 1:
-            distance += dist_matrix[l[i]][l[i + 1]]
-            time += dur_matrix[l[i]][l[i + 1]]
-        time += collection_time[l[i]]
-    return distance, time
-
-
-def has_duplicates(lst):
-    return len(lst) != len(set(lst))
-
-def ispermutation(l):
-    return l[0] == 0 and l[-1] == init_solu[-1]
-
-
 if __name__ == "__main__":
+    # Initialize lists to store the best scores and variances
     best_scores = []
     variances = []
+
+    # Run the genetic algorithm and get the best solution
     best_solution = genetic_algorithm(init_solu, POPULATION_SIZE, best_scores, variances)
 
+    # Create lists for the generation numbers and fitness values
     generation = [i for i in range(len(best_scores))]
     fitness = [s for s in best_scores]
 
+    # Print the best score ever obtained
     print(f"La meilleure solutions jamais obtenue est : {min(best_scores)}")
 
+    # Print the best solution
     print(best_solution)
-    distance, temps = calculateDandT(best_solution)
-    print(f"Distance: {distance / 1000} km, Temps: {temps / 3600} h")
-    print(f"Fitness: {distance + temps}")
-    print(has_duplicates(best_solution))
-    print(f"Est-ce une bonne solution ? {ispermutation(best_solution)}")
 
-    distance, temps = calculateDandT(init_solu)
+    # Calculate and print the distance and time for the best solution
+    distance, temps = calculateDandT(best_solution, dist_matrix, dur_matrix, collection_time)
     print(f"Distance: {distance / 1000} km, Temps: {temps / 3600} h")
+
+    # Print the fitness of the best solution
+    print(f"Fitness: {distance + temps}")
+
+    # Check and print if the best solution has duplicates
+    print(has_duplicates(best_solution))
+
+    # Check and print if the best solution is a permutation
+    print(f"Est-ce une bonne solution ? {ispermutation(best_solution, init_solu)}")
+
+    # Calculate and print the distance and time for the initial solution
+    distance, temps = calculateDandT(init_solu, dist_matrix, dur_matrix, collection_time)
+    print(f"Distance: {distance / 1000} km, Temps: {temps / 3600} h")
+
+    # Print the fitness of the initial solution
     print(f"fitness sol initiale : {distance + temps}")
 
+    # Plot the fitness values over generations
     plt.scatter(generation, fitness)
     plt.show()
---
+++ Code/utils.py
@@ -0,0 +1,114 @@
+import pickle
+
+def load_data(file_name):
+    """
+    Load data from a pickle file.
+
+    Parameters:
+    file_name (str): The name of the file to load.
+
+    Returns:
+    The data loaded from the file.
+    """
+    with open(file_name, "rb") as f:
+        return pickle.load(f)
+
+def transform_data(data):
+    """
+    Transpose a DataFrame and convert it to a flat list.
+
+    Parameters:
+    data (DataFrame): The DataFrame to transform.
+
+    Returns:
+    A flat list containing the values of the DataFrame.
+    """
+    data = data.transpose()
+    data = data.values.tolist()
+    return [item for sublist in data for item in sublist]
+
+def create_dict(pairs):
+    """
+    Create a dictionary from a list of pairs.
+
+    Each pair is added to the dictionary twice, once with the first element as the key and the second as the value, and once with the second element as the key and the first as the value.
+
+    Parameters:
+    pairs (list): The list of pairs.
+
+    Returns:
+    dict: The created dictionary.
+    """
+    pair_dict = {}
+    for pair in pairs:
+        pair_dict[pair[0]] = pair[1]
+        pair_dict[pair[1]] = pair[0]
+    return pair_dict
+
+def calculateDandT(l, dist_matrix, dur_matrix, collection_time):
+    """
+    Calculate the total distance and time for a given route.
+
+    The function iterates over the route and for each pair of consecutive locations, it adds the distance and time between them to the total. It also adds the collection time at each location to the total time.
+
+    Parameters:
+    l (list): The route.
+    dist_matrix (list of list of int): The distance matrix.
+    dur_matrix (list of list of int): The duration matrix.
+    collection_time (list of int): The collection times.
+
+    Returns:
+    tuple: The total distance and time for the route.
+    """
+    distance = 0
+    time = 0
+    for i in range(len(l)):
+        if i != len(l) - 1:
+            distance += dist_matrix[l[i]][l[i + 1]]
+            time += dur_matrix[l[i]][l[i + 1]]
+        time += collection_time[l[i]]
+    return distance, time
+
+
+def has_duplicates(lst):
+    """
+    Check if a list has duplicates.
+
+    The function creates a set from the list, which removes any duplicates. If the length of the set is different from the length of the list, the list has duplicates.
+
+    Parameters:
+    lst (list): The list to check.
+
+    Returns:
+    bool: True if the list has duplicates, False otherwise.
+    """
+    return len(lst) != len(set(lst))
+
+
+def ispermutation(l, init_solu):
+    """
+    Check if a list is a permutation.
+
+    A list is a permutation if it starts and ends with the same elements as the initial solution.
+
+    Parameters:
+    l (list): The list to check.
+    init_solu (list): The initial solution.
+
+    Returns:
+    bool: True if the list is a permutation, False otherwise.
+    """
+    return l[0] == 0 and l[-1] == init_solu[-1]
+
+def calculateVariance(population):
+    """
+    Calculate the variance of the fitness values in the population.
+
+    Parameters:
+    population (list): The population.
+
+    Returns:
+    float: The variance of the fitness values.
+    """
+    mean = sum(sol[0][0] for sol in population) / len(population)
+    return sum((sol[0][0] - mean) ** 2 for sol in population) / len(population)
